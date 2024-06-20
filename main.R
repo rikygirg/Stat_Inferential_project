64ce93c00028754021e1cffdcfab3d1d6362758f
matte <matte.campa@yahoo.it>
2024-06-20 13:12:56 +0200

    scatter_plot script to plot radius vs am and luminosity vs am in a fancy way. Plot saved in picture absolute_magnitude.png

--- main.R
+++ main.R
@@ -2,6 +2,15 @@ library(arm)
 library(ResourceSelection)
 library(pROC)
 library(PRROC)
+library(GGally)
 
-data = read.table("Stars.csv",head=TRUE)
-summary(data)
+data = read.csv("Stars.csv")
+str(data)
+
+df <- data.frame(data["R"],data["L"])
+plot(df, xlab = "Radius", ylab = "Luminosity", main = "Luminosity vs Radius")
+
+# Maybe two categories?
+idx = which(data["R"]<250)
+
+ggpairs(data,columns = 1:4)
